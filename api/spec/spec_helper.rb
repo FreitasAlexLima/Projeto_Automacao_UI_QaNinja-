3d67532a554368f38365bc269f5f512e37dffd32
FreitasAlexLima <84200666+FreitasAlexLima@users.noreply.github.com>
2022-11-11 17:27:07 -0300

    Api automatizando cadastro prévio de usuarios

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -4,6 +4,11 @@ require_relative "routes/signup"
 require_relative "libs/mongo"
 require_relative "routes/equipos"
 
+require 'digest/md5' # essa lib faz a conversão do string para md5 (criptografia)
+
+def to_md5(pass)
+  return Digest::MD5.hexdigest(pass)  
+end
 
 RSpec.configure do |config|
 
@@ -17,4 +22,16 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.before(:suite) do # criamos gancho (before) que será incluido uma unica vez nos sempre que o rspec for rodado
+    users =[  # criamos uma array contendo todos a carga de usuarios que será utilizado para os testes
+      {name: "Alex Lima", email: "alexfreitas468@gmail.com" , password: to_md5("pwd123")},
+      {name: "Teste", email: "testesEquipo@uol.com.br" , password: to_md5("pwd123")},
+      {name: "Lurdinha", email: "Lurdinha@gmail.com" , password: to_md5("pwd123")},
+      {name: "Locador", email: "locador@gmail.com" , password: to_md5("pwd123")},
+      {name: "Eduardo", email: "eduardo@bol.com.br" , password: to_md5("pwd123")},
+    ]
+
+    MongoDB.new.drop_danger # intanciamos o metodo de exclusão dos usuarios ( caso tenha alguma no banco).
+    MongoDB.new.insert_users(users) # e realizamos a intancia do metodo de inclusão dos usuarios 'sementes' para que os testes do rspec sejam realizados.
+  end
 end
